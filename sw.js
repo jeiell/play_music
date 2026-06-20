e3046aa982a68639876187ce637775858c6c78d1
Natanael Alencar Teixeira <jeielmissoes@gmail.com>
2026-06-20 12:22:33 -0300

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('install', (e) => {
-  console.log('Service Worker instalado!');
-});
-
-self.addEventListener('fetch', (e) => {
-  // O navegador exige que o evento fetch seja escutado
-  e.respondWith(fetch(e.request));
-});
